ead4114b9468f1ca20ada54887b5aa6f316d48d9
leandrocodes <leandrovianacodes@gmail.com>
2020-11-25 12:46:15 -0400

    :construction: card component

---
+++ src/components/card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+    <div class="actions">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0 1px 1px darkgrey;
+  transition: 200ms;
+}
+
+.card img {
+  width: 100%;
+}
+
+.title,
+.content,
+.actions {
+  padding: 16px;
+  text-transform: capitalize;
+  text-align: center;
+}
+
+.title,
+.content {
+  border-bottom: 1px solid silver;
+}
+
+.card:hover {
+  transition: 200ms;
+  box-shadow: 0px 1px 3px darkgrey;
+}
+</style>
