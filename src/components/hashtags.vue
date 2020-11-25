6fb95410e97f98e3da682669f080ccbd65f02d6c
leandrocodes <leandrovianacodes@gmail.com>
2020-11-25 14:02:46 -0400

    :construction: hashtags components

---
+++ src/components/hashtags.vue
@@ -0,0 +1,15 @@
+<template>
+  <span v-for="tag in hashtags" :key="tag"> #{{ tag }} </span>
+</template>
+
+<script>
+export default {
+  props: {
+    hashtags: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style></style>
