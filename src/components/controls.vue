3af35d9bdf3fbaa64963adee4f89eeab94100ccb
leandrocodes <leandrovianacodes@gmail.com>
2020-11-25 13:59:22 -0400

    :construction: constrols components

---
+++ src/components/controls.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button>
+      {{ post.likes }}
+    </button>
+    <hashtags :hashtags="post.hashtags"></hashtags>
+  </div>
+</template>
+
+<script>
+import hashtag from './hashtags.vue'
+export default {
+  components: {
+    hashtags
+  },
+  props: {
+    post: {
+      type: Object
+    }
+  },
+  setup(props) {
+    console.log(props.post.title)
+  }
+}
+</script>
+
+<style></style>
