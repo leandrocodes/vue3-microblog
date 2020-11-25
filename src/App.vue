8edf11b771ac1b8a0b0b2028b62e9e57292625c3
leandrocodes <leandrovianacodes@gmail.com>
2020-11-25 14:02:21 -0400

    :construction: using constrols and card component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <div 
-    v-for="post in store.state.posts" 
-    :key="post.id"
-  >
-    {{ post.title }}
+  <div class="cards">
+    <card v-for="post in store.state.posts" :key="post.id">
+      <template v-slot:title>
+        {{ post.title }}
+      </template>
+      <template v-slot:content>
+        {{ post.content }}
+      </template>
+      <template v-slot:actions>
+        <controls :post="post" />
+      </template>
+    </card>
   </div>
 </template>
 
 <script>
 import { store } from './composables/Store'
+import card from './components/card.vue'
+import controls from './components/controls.vue'
 export default {
+  components: {
+    card,
+    controls
+  },
   setup() {
     return { store }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.cards {
+  display: flex;
+}
 </style>
